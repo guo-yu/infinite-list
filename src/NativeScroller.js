f8d5708349ec471d9c8e23b9db565a19ceecf475
Ya Zhuang <zhuangya@bytedance.com>
2016-09-02 17:13:42 +0800

    add: scrolling event ticking

--- src/NativeScroller.js
+++ src/NativeScroller.js
@@ -2,8 +2,20 @@ var StyleHelpers = require('./StyleHelpers');
 
 var NativeScroller = function (scrollElement, callback) {
 
+    var ticking = false;
+
+    function requestTick() {
+        if(!ticking) {
+            requestAnimationFrame(function () {
+                callback(scrollElement.scrollTop);
+                ticking = false;
+            })
+        }
+        ticking = true;
+    }
+
     scrollElement.addEventListener('scroll', function(){
-        callback(scrollElement.scrollTop);
+        requestTick();
     });
 
     return {
